0bf474055fa9a9c80fe9389e5522fe36cb72b537
David Gasinski <dgasinski1@sheffield.ac.uk>
2024-01-04 03:19:05 +0000

    started work on projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,19 @@ import {
   Container,
   Grid,
   GridItem,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  HStack
 } from "@chakra-ui/react";
 import { Header } from "./components/Header";
 import { Socials } from "./components/Socials";
 import { Link } from "@chakra-ui/next-js";
-import { FaRegFilePdf } from "react-icons/fa";
+import { FaGithubSquare, FaRegFilePdf, FaRocket } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { SiNextdotjs, SiChakraui, SiPython, SiSocketdotio } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -85,6 +92,27 @@ export default function Home() {
             <Header underlined={false} size="2rem">
               Projects
             </Header>
+            <Project
+              title="MiniPlayArena"
+              demo="https://miniplayarena.gasinski.dev"
+              github="https://github.com/MiniPlayArena/"
+            >
+              A simple platform to allow users to play popular board games online with their friends. Players can create parties to allow their friends to join and play many classic card/board games! 
+                <HStack pt={5} maxW='50%'>
+                  <Button isActive={false} leftIcon={<SiNextdotjs />}>
+                    NextJS  
+                  </Button>
+                  <Button isActive={false} leftIcon={<SiChakraui />}>
+                    Chakra UI
+                  </Button>
+                  <Button isActive={false} leftIcon={<SiPython />}>
+                    Python Flask
+                  </Button>
+                  <Button isActive={false} leftIcon={<SiSocketdotio />}>
+                    Flask-Socket.io
+                  </Button>
+                </HStack>
+            </Project>
           </GridItem>
         </Grid>
       </motion.div>
@@ -92,6 +120,40 @@ export default function Home() {
   );
 }
 
-function Project({ children }: { children: React.ReactNode }) {
-  return <></>;
+function Project({
+  children,
+  title,
+  github,
+  demo,
+}: {
+  children: React.ReactNode;
+  title: string;
+  github: string;
+  demo: string;
+}) {
+  return (
+    <Card
+      mt={5}
+      mb={5}
+      ml={10}
+      mr={10}
+      bg="highlight.dracula"
+      color="text.dracula"
+    >
+      <CardHeader>
+        <Heading size="lg"> {title}</Heading>
+      </CardHeader>
+      <CardBody>{children}</CardBody>
+      <CardFooter display="flex" justifyContent="center">
+        <Stack direction="row" align="center" spacing={4}>
+          <Link href={demo}>
+            <Button rightIcon={<FaRocket />}>Try it </Button>
+          </Link>
+          <Link href={github}>
+            <Button rightIcon={<FaGithubSquare />}>View on github</Button>
+          </Link>
+        </Stack>
+      </CardFooter>
+    </Card>
+  );
 }
